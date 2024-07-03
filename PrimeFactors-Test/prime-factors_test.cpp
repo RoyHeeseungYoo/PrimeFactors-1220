9551241f3a41003ae4c7c0b724565fedcf00fc0d
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-03 10:58:20 +0900

    [feature] 1

--- PrimeFactors-Test/prime-factors_test.cpp
+++ PrimeFactors-Test/prime-factors_test.cpp
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include "../PrimeFactors/prime-factors.cpp"
 
+#include <vector>
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
